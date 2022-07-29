6402d38bb4a26ff8f2720a7a6fe29f9372b410d8
David Andlinger <david.andlinger@instant-it.at>
2022-07-29 11:40:40 +0200

    removed unused tests

--- integration-tests/src/test/java/io/quarkiverse/apistax/it/ApistaxResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.apistax.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ApistaxResourceIT extends ApistaxResourceTest {
-}
